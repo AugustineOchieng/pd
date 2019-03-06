a506cf1770b341e7f96e7b178595bef78cff27ad
AugustineOchieng <gusochieng@gmail.com>
2019-03-06 09:33:32 +0300

    create prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ gameInit();
 var gamePlay, player, roundScore, totalScore, previousDice1, previousDice2, goal;
 
 document.getElementById("newGame").addEventListener("click", gameInit);
+
+function gameInit() {
+  gamePlay = true;
+  player = Math.floor(Math.random() * 2);
+  roundScore = 0;
+  totalScore = [0, 0];
+  goal = 100;
